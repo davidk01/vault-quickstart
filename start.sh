f0c960e97f5d142ee8953da001fd00908dbcb18b
david karapetyan <dkarapetyan@scriptcrafty.com>
2017-05-19 09:32:31 -0700

    use full path and dump log file

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ source ./vars.sh
 
 # start if not running
 if ! (ps aux | grep [v]ault); then
-  nohup sudo su "${VAULT_USER}" -c "vault server -config \"${VAULT_CONFIG_FILE}\"" &> "${VAULT_LOG_FILE}" &
+  vault_binary="$(which vault)"
+  nohup sudo su "${VAULT_USER}" -c "${vault_binary} server -config \"${VAULT_CONFIG_FILE}\"" &> "${VAULT_LOG_FILE}" &
+  sleep 5
+  cat "${VAULT_LOG_FILE}"
   ps aux | grep [v]ault
 fi
