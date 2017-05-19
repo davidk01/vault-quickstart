c38f724a8917039ecc1757f86c507650e85cfe8b
david karapetyan <dkarapetyan@scriptcrafty.com>
2017-05-19 09:55:24 -0700

    tell user what is happening

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ source ./vars.sh
 vault_binary="$(which vault)"
 if ! (ps aux | grep "${vault_binary}" | grep -v grep); then
   nohup sudo su "${VAULT_USER}" -c "${vault_binary} server -config \"${VAULT_CONFIG_FILE}\"" &> "${VAULT_LOG_FILE}" &
+  echo "Waiting for vault server to start."
   sleep 5
+  echo "Dumping server log file."
   cat "${VAULT_LOG_FILE}"
 fi
