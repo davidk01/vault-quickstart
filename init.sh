88dd06ab4336ba22adc590cedc4444dfbfc3bd99
david karapetyan <dkarapetyan@scriptcrafty.com>
2017-05-19 09:37:39 -0700

    dump vault init file

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ source ./vars.sh
 
 if [[ -e "${VAULT_INIT_FILE}" ]]; then
   echo "Vault initialization file exists so assuming already initialized."
+  cat "${VAULT_INIT_FILE}"
   exit 0
 fi
 vault init -tls-skip-verify &> "${VAULT_INIT_FILE}"
