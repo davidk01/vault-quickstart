e647fd8fae3f4d5790eb97233107157148c9ef59
david karapetyan <dkarapetyan@scriptcrafty.com>
2017-05-19 09:44:35 -0700

    more robust checking for start command

--- start.sh
+++ start.sh
@@ -3,10 +3,9 @@ set -o pipefail
 source ./vars.sh
 
 # start if not running
-if ! (ps aux | grep [v]ault); then
-  vault_binary="$(which vault)"
+vault_binary="$(which vault)"
+if ! (ps aux | grep "${vault_binary}" | grep -v grep); then
   nohup sudo su "${VAULT_USER}" -c "${vault_binary} server -config \"${VAULT_CONFIG_FILE}\"" &> "${VAULT_LOG_FILE}" &
   sleep 5
   cat "${VAULT_LOG_FILE}"
-  ps aux | grep [v]ault
 fi
