d96abf85eba9993c42ce50d7c2fff5090649379c
david karapetyan <dkarapetyan@scriptcrafty.com>
2017-05-19 09:25:26 -0700

    show process output after starting vault

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ set -o pipefail
 source ./vars.sh
 
 # start if not running
-if ! (ps aux | grep [v]ault &> /dev/null); then
+if ! (ps aux | grep [v]ault); then
   nohup sudo su "${VAULT_USER}" -c "vault server -config \"${VAULT_CONFIG_FILE}\"" &> "${VAULT_LOG_FILE}" &
+  ps aux | grep [v]ault
 fi
