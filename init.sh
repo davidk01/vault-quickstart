15ab30f7dc57644011cf3b40cd5f3639c88d03cd
david karapetyan <dkarapetyan@scriptcrafty.com>
2017-05-19 09:39:27 -0700

    always initialize

--- init.sh
+++ init.sh
@@ -2,9 +2,4 @@
 set -o pipefail
 source ./vars.sh
 
-if [[ -e "${VAULT_INIT_FILE}" ]]; then
-  echo "Vault initialization file exists so assuming already initialized."
-  cat "${VAULT_INIT_FILE}"
-  exit 0
-fi
-vault init -tls-skip-verify &> "${VAULT_INIT_FILE}"
+vault init -tls-skip-verify &> "${VAULT_INIT_FILE}" || true
