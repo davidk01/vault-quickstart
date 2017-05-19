252b7e220af65de9812badf2f54060f52812fe0b
david karapetyan <dkarapetyan@scriptcrafty.com>
2017-05-19 10:07:23 -0700

    tell user what is going on

--- unseal.sh
+++ unseal.sh
@@ -3,6 +3,7 @@ set -o pipefail
 source ./vars.sh
 
 # assuming default of 5 shares with 3 required to unseal
+echo "Unsealing vault."
 vault unseal -tls-skip-verify "$(cat "${VAULT_INIT_FILE}" | grep 'Key 1' | awk -F': ' '{print $2}')"
 vault unseal -tls-skip-verify "$(cat "${VAULT_INIT_FILE}" | grep 'Key 2' | awk -F': ' '{print $2}')"
 vault unseal -tls-skip-verify "$(cat "${VAULT_INIT_FILE}" | grep 'Key 3' | awk -F': ' '{print $2}')"
